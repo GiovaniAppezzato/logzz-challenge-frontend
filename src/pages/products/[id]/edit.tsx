b4967f7d5f7666c0abbabcf060642f9894c70e35
GiovaniAppezzato <giovani.apztt@gmail.com>
2024-09-19 02:50:12 -0300

    feat: added loading on edit product page

--- src/pages/products/[id]/edit.tsx
+++ src/pages/products/[id]/edit.tsx
@@ -5,7 +5,7 @@ import * as yup from "yup";
 import { useTranslation } from 'react-i18next';
 import PrivateLayout from "@/components/layouts/PrivateLayout";
 import ToastService from '@/services/toast';
-import { Card, Input, Label, Button, Avatar } from "@/components";
+import { Card, Input, Label, Button, Avatar, Alert, Spinner } from "@/components";
 import i18n from '@/locales/i18n';
 import translationsYup from '@/locales/yupMessages';
 import ProductService from '@/services/api/product';
@@ -53,7 +53,10 @@ export default function EditProductPage() {
         const response = await ProductService.getProduct(router.query.id as string);
         const product = response.data.data;
 
-        setProduct(product);product
+        // Set product without changed fields
+        setProduct(product);
+        
+        // Set form values
         setValues({
           name: product.name,
           description: product.description,
@@ -134,84 +137,92 @@ export default function EditProductPage() {
           <Link href="/users"></Link>
         </Card.Header>
         <Card.Body>
-        <div className="mb-8 mt-4">
-            <div className="w-full flex flex-wrap justify-center items-start sm:pl-4 md:flex-nowrap">
-              <div className="relative w-max">
-                <Avatar 
-                  src={values.image || undefined} 
-                  icon={<FaImage className="w-6 h-6 m-auto text-gray-600" />} 
-                  className="!w-24 !h-24 !text-xl" 
-                />
-                <label htmlFor='image' className="button-icon button-primary button-icon-lg cursor-pointer rounded-full absolute -bottom-0.5 -right-0.5 shadow-none border-[3px] border-navbar">
-                  <FaCamera size={12} />
-                </label>
-                
-                <input 
-                  type="file" 
-                  className="hidden"
-                  accept="image/*"
-                  name="image"
-                  id="image"
-                  onChange={onChangePhoto}
-                />
+          {!isLoading ? (
+            <>
+              <div className="mb-8 mt-4">
+                <div className="w-full flex flex-wrap justify-center items-start sm:pl-4 md:flex-nowrap">
+                  <div className="relative w-max">
+                    <Avatar 
+                      src={values.image || undefined} 
+                      icon={<FaImage className="w-6 h-6 m-auto text-gray-600" />} 
+                      className="!w-24 !h-24 !text-xl" 
+                    />
+                    <label htmlFor='image' className="button-icon button-primary button-icon-lg cursor-pointer rounded-full absolute -bottom-0.5 -right-0.5 shadow-none border-[3px] border-navbar">
+                      <FaCamera size={12} />
+                    </label>
+                    
+                    <input 
+                      type="file" 
+                      className="hidden"
+                      accept="image/*"
+                      name="image"
+                      id="image"
+                      onChange={onChangePhoto}
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
 
-          <div className="flex flex-wrap -mx-2">
-            <div className="w-full px-2 mb-4 md:w-1/3">
-              <Label className="px-1" htmlFor="name">{t("fields.nameProduct")} <span className='text-danger'>*</span></Label>
+              <div className="flex flex-wrap -mx-2">
+                <div className="w-full px-2 mb-4 md:w-1/3">
+                  <Label className="px-1" htmlFor="name">{t("fields.nameProduct")} <span className='text-danger'>*</span></Label>
 
-              <Input 
-                name='name'
-                id='name'
-                type='text'
-                value={values.name}
-                onChange={(event) => setValues({ ...values, name: event.target.value })}
-              />
-            </div>
-            <div className="w-full px-2 mb-4 md:w-1/3">
-              <Label className="px-1" htmlFor="name">{t("fields.price")} <span className='text-danger'>*</span></Label>
+                  <Input 
+                    name='name'
+                    id='name'
+                    type='text'
+                    value={values.name}
+                    onChange={(event) => setValues({ ...values, name: event.target.value })}
+                  />
+                </div>
+                <div className="w-full px-2 mb-4 md:w-1/3">
+                  <Label className="px-1" htmlFor="name">{t("fields.price")} <span className='text-danger'>*</span></Label>
 
-              <Input 
-                name='price'
-                id='price'
-                type='number'
-                value={values.price}
-                onChange={(event) => setValues({ ...values, price: event.target.value })}
-              />
-            </div>
-            <div className="w-full px-2 mb-4 md:w-1/3">
-              <Label className="px-1" htmlFor="name">{t("fields.category")} <span className='text-danger'>*</span></Label>
+                  <Input 
+                    name='price'
+                    id='price'
+                    type='number'
+                    value={values.price}
+                    onChange={(event) => setValues({ ...values, price: event.target.value })}
+                  />
+                </div>
+                <div className="w-full px-2 mb-4 md:w-1/3">
+                  <Label className="px-1" htmlFor="name">{t("fields.category")} <span className='text-danger'>*</span></Label>
 
-              <Input
-                name='category'
-                id='category'
-                type='text'
-                value={values.category}
-                onChange={(event) => setValues({ ...values, category: event.target.value })}
-              />
-            </div>
-          </div>
-          <div className="flex flex-wrap -mx-2">
-            <div className="w-full px-2 mb-4 md:w-full">
-              <Label className="px-1" htmlFor="name">{t("fields.description")} <span className='text-danger'>*</span></Label>
+                  <Input
+                    name='category'
+                    id='category'
+                    type='text'
+                    value={values.category}
+                    onChange={(event) => setValues({ ...values, category: event.target.value })}
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-2">
+                <div className="w-full px-2 mb-4 md:w-full">
+                  <Label className="px-1" htmlFor="name">{t("fields.description")} <span className='text-danger'>*</span></Label>
 
-              <textarea
-                className='input'
-                name='description'
-                id='description'
-                value={values.description}
-                onChange={(event) => setValues({ ...values, description: event.target.value })}
-                rows={3}
-              />
-            </div>
-          </div>
-          <div className="flex justify-end items-center gap-x-4 mt-4">
-            <Button className='normal-case' size="sm" variant='primary' loading={isUpdating} onClick={handleSubmit}>
-              {t("pages.editProduct.save")}
-            </Button>
-          </div>
+                  <textarea
+                    className='input'
+                    name='description'
+                    id='description'
+                    value={values.description}
+                    onChange={(event) => setValues({ ...values, description: event.target.value })}
+                    rows={3}
+                  />
+                </div>
+              </div>
+              <div className="flex justify-end items-center gap-x-4 mt-4">
+                <Button className='normal-case' size="sm" variant='primary' loading={isUpdating} onClick={handleSubmit}>
+                  {t("pages.editProduct.save")}
+                </Button>
+              </div>
+            </>
+          ) : (
+            <Alert className='w-full flex justify-center'>
+              <Spinner className='h-5 w-5' />
+            </Alert>
+          )}
         </Card.Body>
       </Card>
     </PrivateLayout>
